a41b30e719c91d712a7dd637196199514e044ac2
Benjamin H <benjamin.hallowitz@gmail.com>
2022-08-11 18:28:04 -0700

    Update ore init and tag usage for 1.18.2

--- src/main/java/tv/mapper/roadstuff/data/RSTags.java
+++ src/main/java/tv/mapper/roadstuff/data/RSTags.java
@@ -4,6 +4,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.Tag;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import tv.mapper.roadstuff.RoadStuff;
@@ -12,53 +13,53 @@ public class RSTags
 {
     public static class Blocks
     {
-        public static final Tag.Named<Block> CONCRETE_ROAD = tag("concrete_road");
-        public static final Tag.Named<Block> ASPHALT_ROAD = tag("asphalt_road");
-        public static final Tag.Named<Block> REFLECTORS = tag("reflectors");
-        public static final Tag.Named<Block> CONCRETE = tag("concrete");
-        public static final Tag.Named<Block> ASPHALT = tag("asphalt");
+        public static final TagKey<Block> CONCRETE_ROAD = tag("concrete_road");
+        public static final TagKey<Block> ASPHALT_ROAD = tag("asphalt_road");
+        public static final TagKey<Block> REFLECTORS = tag("reflectors");
+        public static final TagKey<Block> CONCRETE = tag("concrete");
+        public static final TagKey<Block> ASPHALT = tag("asphalt");
 
-        private static Tag.Named<Block> tag(String name)
+        private static TagKey<Block> tag(String name)
         {
-            return BlockTags.bind(new ResourceLocation(RoadStuff.MODID, name).toString());
+            return BlockTags.create(new ResourceLocation(RoadStuff.MODID, name));
         }
     }
 
     public static class Items
     {
-        public static final Tag.Named<Item> CONCRETE_ROAD = tag("concrete_road");
-        public static final Tag.Named<Item> ASPHALT_ROAD = tag("asphalt_road");
-        public static final Tag.Named<Item> REFLECTORS = tag("reflectors");
-        public static final Tag.Named<Item> CONCRETE = tag("concrete");
-        public static final Tag.Named<Item> ASPHALT = tag("asphalt");
+        public static final TagKey<Item> CONCRETE_ROAD = tag("concrete_road");
+        public static final TagKey<Item> ASPHALT_ROAD = tag("asphalt_road");
+        public static final TagKey<Item> REFLECTORS = tag("reflectors");
+        public static final TagKey<Item> CONCRETE = tag("concrete");
+        public static final TagKey<Item> ASPHALT = tag("asphalt");
 
-        private static Tag.Named<Item> tag(String name)
+        private static TagKey<Item> tag(String name)
         {
-            return ItemTags.bind(new ResourceLocation(RoadStuff.MODID, name).toString());
+            return ItemTags.create(new ResourceLocation(RoadStuff.MODID, name));
         }
     }
 
     public static class ForgeBlocks
     {
-        public static final Tag.Named<Block> STORAGE_BLOCKS_BITUMEN = tag("storage_blocks/bitumen");
-        public static final Tag.Named<Block> ORES_BITUMEN = tag("ores/bitumen");
+        public static final TagKey<Block> STORAGE_BLOCKS_BITUMEN = tag("storage_blocks/bitumen");
+        public static final TagKey<Block> ORES_BITUMEN = tag("ores/bitumen");
 
-        private static Tag.Named<Block> tag(String name)
+        private static TagKey<Block> tag(String name)
         {
-            return BlockTags.bind(new ResourceLocation("forge", name).toString());
+            return BlockTags.create(new ResourceLocation("forge", name));
         }
     }
 
     public static class ForgeItems
     {
-        public static final Tag.Named<Item> STORAGE_BLOCKS_BITUMEN = tag("storage_blocks/bitumen");
-        public static final Tag.Named<Item> ORES_BITUMEN = tag("ores/bitumen");
+        public static final TagKey<Item> STORAGE_BLOCKS_BITUMEN = tag("storage_blocks/bitumen");
+        public static final TagKey<Item> ORES_BITUMEN = tag("ores/bitumen");
 
-        public static final Tag.Named<Item> BITUMEN = tag("bitumen");
+        public static final TagKey<Item> BITUMEN = tag("bitumen");
 
-        private static Tag.Named<Item> tag(String name)
+        private static TagKey<Item> tag(String name)
         {
-            return ItemTags.bind(new ResourceLocation("forge", name).toString());
+            return ItemTags.create(new ResourceLocation("forge", name));
         }
     }
 }
--- src/main/java/tv/mapper/roadstuff/world/RSOres.java
+++ src/main/java/tv/mapper/roadstuff/world/RSOres.java
@@ -1,27 +1,50 @@
 package tv.mapper.roadstuff.world;
 
-import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.features.OreFeatures;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 import tv.mapper.mapperbase.world.BaseOreGenerator;
 import tv.mapper.mapperbase.world.CustomOre;
 import tv.mapper.mapperbase.world.OreList;
 import tv.mapper.roadstuff.config.RSConfig.CommonConfig;
 import tv.mapper.roadstuff.world.level.block.RSBlockRegistry;
 
+import java.util.List;
+
 public class RSOres
 {
-    public static PlacedFeature BITUMEN = PlacementUtils.register("bitumen", FeatureUtils.register("bitumen", Feature.ORE.configured(new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, RSBlockRegistry.BITUMEN_ORE.get().defaultBlockState(), CommonConfig.BITUMEN_SIZE.get()))).placed(BaseOreGenerator.commonOrePlacement(CommonConfig.BITUMEN_CHANCE.get(), HeightRangePlacement.uniform(VerticalAnchor.absolute(CommonConfig.BITUMEN_MIN_HEIGHT.get()), VerticalAnchor.absolute(CommonConfig.BITUMEN_MAX_HEIGHT.get())))));
+    static OreConfiguration bitumenConfig = new OreConfiguration(
+            OreFeatures.STONE_ORE_REPLACEABLES,
+            RSBlockRegistry.BITUMEN_ORE.get().defaultBlockState(),
+            CommonConfig.BITUMEN_SIZE.get()
+    );
+
+    public static Holder<PlacedFeature> BITUMEN = registerPlacedOreFeature("bitumen", new ConfiguredFeature<>(Feature.ORE, bitumenConfig),
+            BaseOreGenerator.commonOrePlacement(
+                    CommonConfig.BITUMEN_CHANCE.get(),
+                    HeightRangePlacement.uniform(
+                            VerticalAnchor.absolute(CommonConfig.BITUMEN_MIN_HEIGHT.get()),
+                            VerticalAnchor.absolute(CommonConfig.BITUMEN_MAX_HEIGHT.get())
+                    )
+            )
+    );
 
     public static final CustomOre bitumen = new CustomOre("bitumen", BITUMEN, CommonConfig.BITUMEN_GENERATION.get(), CommonConfig.BITUMEN_WHITELIST_MODE.get(), CommonConfig.BITUMEN_BIOME_LIST.get());
 
     public static void initOres()
     {
         OreList.addOre(bitumen);
+    }
+
+    private static <C extends FeatureConfiguration, F extends Feature<C>> Holder<PlacedFeature> registerPlacedOreFeature(String registryName, ConfiguredFeature<C, F> feature, List<PlacementModifier> placementModifiers) {
+        return PlacementUtils.register(registryName, Holder.direct(feature), placementModifiers);
     }
 }
